f31fffe2123853b5f8407bc788c86599e6c42565
kkimseunghwan <rhian3493@gmail.com>
2025-03-14 15:40:54 +0900

    0314 Boot Study

--- Python/Mar13_2_condStmt/p03_shift.py
+++ Python/Mar13_2_condStmt/p03_shift.py
@@ -7,7 +7,7 @@
 
 # value = 1 ~ 15
 
-# shift << 
+# shift << 활용?
 
 # 3 와이파이, 주차장 0011 0100 0001
 # 12 24시간, 흡연실 1100 0001
@@ -20,15 +20,74 @@
 # 2진수? 0001 0010 0100 1000
 
 value = int(input())
+print(bin(value))
 
-# if문 4개
-if (value & 1):
-    print("와이파이")
-if (value & 2):
-    print("주차장")
-if (value & 4):
-    print("흡연실")
-if (value & 8):
-    print("24시간")
+# if문 4개 - 내가 푼 버전.
+if (value & 1): print("와이파이")
+if (value & 2): print("주차장")
+if (value & 4): print("흡연실")
+if (value & 8): print("24시간")
 
+print("-----")
+
+# shift 사용?
+# if value & 1 : print("와이파이")
+# value = value >> 1
+# print(bin(value))
+
+# if value & 1 : print("주차장")
+# value = value >> 1
+# print(bin(value))
+
+# if value & 1 : print("흡연실")
+# value = value >> 1
+# print(bin(value))
+
+# if value & 1 : print("24시간")
+# value = value >> 1
+# print(bin(value))
+
+print("-----")
+
+
+# 선생님 교육용 코드 : (이후 반복문 활용 가능)
+
+
+# if value >= (1 << 3): # 8
+#     print("24시간")
+#     value -= (1 << 3)
+# if value >= (1 << 2): # 4
+#     print("흡연실")
+#     value >= (1 << 2)
+# if value >= (1 << 1): # 2
+#     print("주차장")
+#     value -= (1 << 1)
+# if value >= (1 << 0): # 1
+#     print("와이파이")
+#     value -= (1 << 0)
+
+# print("-----")
+
+# # 아래와 같음
+# if value >= 8: # 8
+#     print("24시간")
+#     value -= 8
+# if value >= 4: # 4
+#     print("흡연실")
+#     value -= 4
+# if value >= 2: # 2
+#     print("주차장")
+#     value -= 2
+# if value >= 1: # 1
+#     print("와이파이")
+#     value -= 1
+
+
+# 반복문 배우고 다시 돌아왔음.
+print_dict = { 3:"24시간", 2:"흡연실", 1:"주차장", 0:"와이파이" }
+
+for x, y in print_dict.items():
+    if value >= (1 << x):
+        print(y)
+        value -= (1 << x)
 
