0413fa1d1c0e7de541358200d564ca7297878156
kkimseunghwan <rhian3493@gmail.com>
2025-03-14 10:08:41 +0900

    first

--- Python/Mar13_2_condStmt/p03_cs.py
+++ Python/Mar13_2_condStmt/p03_cs.py
@@ -7,9 +7,11 @@ value = int(input())
 # 4 흡연실
 # 8 24시간
 
+# value = 1 ~ 15
+
 
 # 3 와이파이, 주차장
-# 12 와이파이, 흡연실
+# 12 24시간, 흡연실
 # 13 흡연실 24시간 와이파이
 # 7이면 흡연실 주차장 와이파이
 # <= 얘는 반복되서 value 값이 0이 될 때까지 출력 되는거
@@ -21,18 +23,18 @@ value = int(input())
 isRepeat = not(value in (1,2,4,8))
 
 while value != 0:
-    if value == 1:
-        value -= 1 
-        print("와이파이")
-    elif value == 2: 
-        value -= 2
-        print("주차장")
-    elif value == 4: 
-        value -= 4
-        print("흡연실")
-    elif value == 8: 
+    if value >= 8:
         value -= 8
         print("24시간")
+    elif value >= 4:
+        value -= 4
+        print("흡연실")
+    elif value >= 2: 
+        value -= 2
+        print("주차장")
+    elif value >= 1: 
+        value -= 1
+        print("와이파이")
 
 
 
