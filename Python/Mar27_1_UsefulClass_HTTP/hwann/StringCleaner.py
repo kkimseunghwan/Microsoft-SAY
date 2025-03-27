39ae547b56c0324cc30f6b3d6c10fbdb61271920
kkimseunghwan <rhian3493@gmail.com>
2025-03-27 18:18:02 +0900

    0327 boot study

---
+++ Python/Mar27_1_UsefulClass_HTTP/hwann/StringCleaner.py
@@ -0,0 +1,11 @@
+
+# 자주 쓰니까 => 따로 정리하자
+
+class StringCleaner:
+    #@staticmethod # 객체를 만들지 않아도 쓸 수 있는 함수
+    def DeleteBold(txt):
+        txt = txt.replace("&quot;", "")
+        txt = txt.replace("<b>", "")
+        txt = txt.replace("</b>", "")
+        return txt
+
