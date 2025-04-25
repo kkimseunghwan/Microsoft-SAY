b764c3fbb751a88f744f384673f63f9782b6e2c4
kkimseunghwan <rhian3493@gmail.com>
2025-04-25 18:25:03 +0900

    0425 boot study

--- Python/Algorithm/test.py
+++ Python/Algorithm/test.py
@@ -1,6 +1,31 @@
-def solution(s):
-    s = s.split(' ')
-    return sum(int(s[i]) if s[i] != 'Z' else -int(s[i-1]) for i in range(len(s)))
 
-print(solution("1 2 Z 3"))
+def solution(numbers):
+    numStr = {
+        "zero": '0',
+        "one": '1',
+        "two": '2',
+        "three": '3',
+        "four": '4',
+        "five": '5',
+        "six": '6',
+        "seven": '7',
+        "eight": '8',
+        "nine": '9'
+    }
+    answer = ''
+    pivot = 0
+    while numbers:
+        if input():
+            print(pivot, numbers[:pivot], "//", numbers, answer)
+            if numbers[:pivot] in numStr:
+                answer += numStr[numbers[:pivot]]
+                numbers = numbers[pivot:]
+                pivot = 0
+                
+            pivot += 1
+    
+    return answer
 
+
+a = "onetwothreefourfivesixseveneightnine"
+print(solution(a))
